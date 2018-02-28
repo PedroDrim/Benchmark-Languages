f9dfa6bf5890c408059fe6b0dd02e91dfbfc31b4
pedrodrim <pedrohenriquedrim@gmail.com>
2018-02-28 13:30:21 -0300

    correção dos graficos

--- outputs/simpleclass/simpleclass_graph.r
+++ outputs/simpleclass/simpleclass_graph.r
@@ -9,12 +9,13 @@ languages  = sapply(languages, function(x){
   return(x[1])
 })
 
-dt = data.frame(time = as.numeric(treatment)/1000, lang = languages)
+dt = data.frame(seconds = as.numeric(treatment)/1000, language = languages)
 
-tiff(filename = "benchmark_simpleclass.tiff")
+png(filename = "benchmark_simpleclass.png")
 
-ggplot(data=dt, aes(x=lang, y=time)) +
+ggplot(data=dt, aes(x=language, y=seconds)) +
   geom_bar(stat="identity", fill="steelblue") +
+  geom_text(aes(label=seconds), vjust=-0.3, size=3.5) +
   theme_minimal()
 
 dev.off()
