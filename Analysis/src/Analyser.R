6e504fe48f46ee1922254e774c26347d64ca922d
pedrodrim <pedrohenriquedrim@gmail.com>
2018-04-30 11:10:49 -0300

    Refatoraçao do experimento SimpleClass

--- Analysis/src/Analyser.R
+++ Analysis/src/Analyser.R
@@ -1,19 +1,40 @@
 # Importando bibioteca para paralelização de tarefas
-require(parallel)
+require(plyr)
 
 # Função para analisar os resultados dos experimentos
-analyseData = function(result) {
+analyseData = function(result, timeScale = "ms") {
 
-  lang = NA
+  #====================================================
+  # Prepare the data
   
-  for(element in result) {
-    if(class(element) == "character") {
-      lang = element
-    
-    } else {
-      
-    }
-  }
+  result = data.frame(result)
   
+  tamanho_array = as.character(result$Tamanho)
+  tamanho_array = gsub("\"", "", tamanho_array)
+  
+  result$Tamanho = tamanho_array
+  
+  tempo_array = as.character(result$Tempo)
+  tempo_array = gsub(timeScale, "", result$Tempo)
+  tempo_array = gsub("\"", "", tempo_array)
+  
+  result$Tempo = as.numeric(tempo_array)
+  
+  #====================================================
+  
+  #====================================================
+  # Analyse the data
+  
+  result$lang = as.character(result$lang)
+  
+  a = ddply(result, c("lang", "Tamanho"), summarise,
+        mean = round(mean(Tempo), 2),
+        sd = round(sd(Tempo), 2))
+  
+  a$Tamanho = as.character(a$Tamanho)
+  a$Tamanho = as.numeric(a$Tamanho)
+  #====================================================
+  
+  return(a)
 }
 
--- Analysis/src/Reader.R
+++ Analysis/src/Reader.R
@@ -1,23 +1,65 @@
-# Importando bibioteca para paralelização de tarefas
-require(parallel)
-
 # Função para executar os experimentos
-readData = function(langDirectory, shellFile = "Bench.sh", times = 10, paramArray = "1000") {
+readData = function(langDirectory, outputTag = "\\[OK\\]", shellFile = "Bench.sh", times = 10, paramArray = "1000") {
   if(times <= 0) times = 1
   
-  lang = strsplit(langDirectory, split = "/")[[1]]
-  result = list()
-  result$lang = lang[length(lang)]
-  
-  for(param in paramArray) {
-    result[[param]] = list()
+  lapply_res = lapply(paramArray, function(param, ot) {
+    
+    #====================================================
+    # Run and get the output
+    
+    r = list()
     
     for(t in 1:times) {
-      
       cmd = sprintf("cd %s && sh %s %s", langDirectory, shellFile, param)
-      result[[param]][[t]] = system(cmd, intern = T)
+      out = system(cmd, intern = T)
+      
+      index = grep(ot, out)
+      r[[t]] = out[index]
     }
-  }
+    
+    #====================================================
+
+    #====================================================
+    # Clean and return the output
+    
+    lang_result = as.data.frame(r)
+    names(lang_result) = NULL
+    
+    start_data = apply(lang_result, 1, function(f_line) {
+      
+      treat = strsplit(f_line, split = ":")
+      treat_matrix = do.call(rbind, treat)
+      
+      return(list(treat_matrix))
+    })
+    
+    size = length(start_data)
+    m_res = matrix(NA, ncol = size, nrow = length(lang_result))
+
+    label_array = c()    
+    for(i in 1:size )  {
+      i_array = start_data[[i]]
+      i_array = i_array[[1]]
+      label = unique(i_array[,1])
+      label = gsub(ot, "", label)
+      
+      m_res[,i] = i_array[,-1]
+      
+      label_array = c(label_array, label)
+    }    
+  
+    colnames(m_res) = label_array
+    return(m_res)
+    
+    #====================================================
+    
+  }, outputTag)
+
+  lang = strsplit(langDirectory, split = "/")[[1]]
+  lang = lang[length(lang)]
+  
+  result = do.call(rbind, lapply_res)
+  result = cbind(lang, result)
   
   return(result)
 }
